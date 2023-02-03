eb8b26b15d9328eabc02f77899e5fccee177291d
huseyindogan00 <huseyindgn00@gmail.com>
2023-02-03 13:30:11 +0300

    DENEME

--- lib/ui/pages/talk/talk_page.dart
+++ lib/ui/pages/talk/talk_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:live_chat_app/data/models/user_model.dart';
 
 class TalkPage extends StatefulWidget {
-  const TalkPage({super.key, required this.currentUser, required this.chatUser});
+  const TalkPage(
+      {super.key, required this.currentUser, required this.chatUser});
 
   final UserModel currentUser;
   final UserModel chatUser;
@@ -28,13 +29,46 @@ class _TalkPageState extends State<TalkPage> {
       appBar: AppBar(
         title: Text('Konuşma'),
       ),
-      body: Column(
-        children: [
-          Expanded(
+      body: Container(
+        color: Colors.amber.shade100,
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: Wrap(
+          runSpacing: 10,
+          children: [
+            Expanded(
               child: ListView(
-            children: [],
-          ))
-        ],
+                children: [
+                  _buildMessageCard(),
+                  _buildMessageCard(),
+                  _buildMessageCard(),
+                  _buildMessageCard(),
+                  _buildMessageCard(),
+                  _buildMessageCard()
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildMessageCard() {
+    return Card(
+      color: Colors.cyan,
+      child: Container(
+        padding: EdgeInsets.all(5),
+        width: double.infinity,
+        child: Wrap(
+          runSpacing: 10,
+          runAlignment: WrapAlignment.end,
+          children: [
+            Text(
+                'asdasdaspoddlkadam poksappoalmjpokphalskdsapdma p omoflmfsı  km'),
+            Text(
+                'asdasdaspoddlkadam poksappoalmjpokphalskdsapdma p omoflmfsı  km'),
+          ],
+        ),
       ),
     );
   }
