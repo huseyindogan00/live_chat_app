209d8680810ac55b6ed78168ccfca0f1b26a2efb
huseyindogan00 <huseyindgn00@gmail.com>
2023-02-09 23:54:44 +0300

    message textleri düzenlendi

--- lib/ui/pages/talk/talk_page.dart
+++ lib/ui/pages/talk/talk_page.dart
@@ -1,12 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:live_chat_app/core/constant/image/image_const_path.dart';
+import 'package:live_chat_app/core/utility/global_util.dart';
 import 'package:live_chat_app/data/models/message_model.dart';
 import 'package:live_chat_app/data/models/user_model.dart';
 import 'package:live_chat_app/ui/viewmodel/user_view_model.dart';
 import 'package:provider/provider.dart';
 
 class TalkPage extends StatefulWidget {
-  const TalkPage({super.key, required this.currentUser, required this.chatUser});
+  const TalkPage(
+      {super.key, required this.currentUser, required this.chatUser});
 
   final UserModel currentUser;
   final UserModel chatUser;
@@ -19,6 +21,7 @@ class _TalkPageState extends State<TalkPage> {
   late final UserModel currentUser;
   late final UserModel chatUser;
   TextEditingController messageTextFieldController = TextEditingController();
+  ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
@@ -32,70 +35,96 @@ class _TalkPageState extends State<TalkPage> {
     UserViewModel _userViewModel = Provider.of<UserViewModel>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(_userViewModel.userModel!.userName.toString()),
+        title: Text(chatUser.userName.toString()),
         centerTitle: true,
       ),
-      body: Container(
-        color: Colors.amber.shade100,
-        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            _buildMessageCard(_userViewModel),
-            _buildMessageLine(_userViewModel),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildMessageCard(UserViewModel userViewModel) {
-    return Expanded(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.75,
-        child: StreamBuilder<List<MessageModel>>(
-          stream: userViewModel.getMessage(currentUser.userID!, chatUser.userID!),
-          builder: (context, AsyncSnapshot<List<MessageModel>> snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            List<MessageModel> messages = snapshot.data!;
-            return ListView.builder(
-              itemCount: messages.length,
-              itemBuilder: (context, index) {
-                MessageModel message = messages[index];
-                return message.fromMe ? _buildCurrentUserMessageText(message) : _buildChatUserMessageText(message);
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<List<MessageModel>>(
+              stream: _userViewModel.getMessage(
+                  currentUser.userID!, chatUser.userID!),
+              builder: (context, AsyncSnapshot<List<MessageModel>> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  List<MessageModel> messages = snapshot.data!;
+                  return ListView.builder(
+                    controller: _scrollController,
+                    itemCount: messages.length,
+                    itemBuilder: (context, int index) {
+                      MessageModel message = messages[index];
+                      if (message.fromMe) {
+                        return _buildCurrentUserMessageText(message);
+                      }
+                      return _buildChatUserMessageText(message);
+                    },
+                  );
+                }
               },
-            );
-          },
-        ),
+            ),
+          ),
+          _buildMessageTextField(_userViewModel),
+        ],
       ),
     );
   }
 
   Widget _buildCurrentUserMessageText(MessageModel message) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Colors.red,
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10),
-          bottomLeft: Radius.circular(10),
-        ),
-      ),
-      margin: const EdgeInsets.only(bottom: 10),
-      child: ListTile(
-        title: Text(message.message),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blueAccent.shade200,
+              shape: BoxShape.rectangle,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+              ),
+            ),
+            margin: const EdgeInsets.only(bottom: 20),
+            child: Text(message.message),
+          ),
+        ],
       ),
     );
   }
 
-  _buildChatUserMessageText(MessageModel message) {}
+  _buildChatUserMessageText(MessageModel message) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.amber,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                  ),
+                ),
+                margin: const EdgeInsets.only(bottom: 20),
+                child: Text(message.message),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
-  Container _buildMessageLine(UserViewModel userViewModel) {
+  Container _buildMessageTextField(UserViewModel userViewModel) {
     return Container(
-      padding: const EdgeInsets.only(bottom: 8, left: 2),
+      padding: const EdgeInsets.all(15),
       child: Row(
         children: [
           Expanded(
@@ -118,11 +147,12 @@ class _TalkPageState extends State<TalkPage> {
                   whoID: chatUser.userID!,
                   fromMe: true,
                   message: messageTextFieldController.text,
+                  date: FieldValue.serverTimestamp(),
                 );
 
                 messageTextFieldController.clear();
 
-                bool result = await userViewModel.saveMessage(messageModel);
+                await userViewModel.saveMessage(messageModel);
               }
             },
             child: const Icon(Icons.navigation_outlined),
