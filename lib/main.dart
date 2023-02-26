11e017b8eb27e25f22eed513eacb53caa590df34
HuseyinDOGAN <huseyindgn00@gmail.com>
2023-02-26 20:04:47 +0300

    firabasemessaging ekleniyor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:live_chat_app/core/init/locator/global_locator.dart';
+import 'package:live_chat_app/ui/pages/home/home_screen.dart';
 import 'package:live_chat_app/ui/pages/start/splash_screen.dart';
 import 'package:live_chat_app/ui/viewmodel/all_users_view_model.dart';
 import 'package:live_chat_app/ui/viewmodel/user_view_model.dart';
@@ -8,9 +11,11 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // başlatıldığından emin ol
-
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(
+      firebaseMessage); // uygulama arka planda çalıştığında data mesajlarımı alabilmek için
   setupLocator();
+
   runApp(const MyApp());
 }
 
